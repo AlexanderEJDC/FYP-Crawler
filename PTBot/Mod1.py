c613927843fe648abdbfaf429721d55389a255cc
Alexander Cock <alexander@TopazLPT-Kl.TopazDomain>
2023-12-02 15:00:05 +0000

    PTBot Prototype complete, fat cut out

---
+++ PTBot/Mod1.py
@@ -0,0 +1,35 @@
+import logging
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
+
+#Configure Logging module - use level=logging.DEBUG for more verbose info.
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
+
+#This is what the application builder actually uses. 
+#Defines a function that processes a specific form of update. 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text="/start Recieved")
+    
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I have recieved your response")
+
+if __name__ == '__main__':
+    #Application builder. On its own, doesn't do anything.
+    application = ApplicationBuilder().token(
+        '6460026166:AAFf7Wv93Cpa9YUFtsZ0E-AJfmFgKGDx_1s').build()
+    
+    #The command handler. This will listen for the "\start"
+    #commands.
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    
+    application.add_handler(start_handler) #Listens for the start command!
+    application.add_handler(echo_handler) #Listens for the second command handler!
+    
+    #This runs the bot until it recieves Cntrl-C from Terminal!
+    application.run_polling() 
---
+++ PTBot/Mod2.py
@@ -0,0 +1,37 @@
+import logging
+import asyncio
+import telegram
+from telegram import Update
+from telegram.ext import MessageHandler, filters, ApplicationBuilder, ContextTypes
+
+#Configure Logging module - use level=logging.DEBUG for more verbose info.
+#logging.basicConfig(
+#    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#    level=logging.DEBUG
+#)
+
+#Reads any messages in an update
+async def ReadAllMsgs(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update._effective_message.text
+    print(message)
+
+
+#Grabs a file from any update
+async def GrabFile(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    incom_file = await update.effective_message.effective_attachment.get_file()
+    await incom_file.download_to_drive('recieved_file')
+    print("File recieved!") 
+
+if __name__ == '__main__':
+    #Application builder. On its own, doesn't do anything.
+    application = ApplicationBuilder().token(
+        '6460026166:AAFf7Wv93Cpa9YUFtsZ0E-AJfmFgKGDx_1s').build()
+    
+    #Command handles!
+    file_handler = MessageHandler(filters.ATTACHMENT & (~filters.COMMAND), GrabFile)
+    channel_handler = MessageHandler(filters.ALL & (~filters.COMMAND), ReadAllMsgs)
+
+    application.add_handler(file_handler)
+    application.add_handler(channel_handler)
+
+    application.run_polling()  #Run until Cntrl-C! 
