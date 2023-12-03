0aff853bfa50577e653342c23f5f1f027ce4a5ec
Alexander Cock <alexcock27@gmail.com>
2023-12-03 14:19:23 +0000

    Added BotMaster.py

---
+++ BotMaster.py
@@ -0,0 +1,14 @@
+#Master Control File 
+
+#Setup relevant imports from other API interfaces that will be required. 
+import asyncio
+from telegram import Mod2
+from VTBot import whoops
+
+#Main function
+async def main():
+    print() #blank for further writing
+
+#Execute main
+if __name__ == "__main__":
+    asyncio.run(main())
