2b87ecd10797474072e9eb73d4b0fd64f8a08e1a
Alexander Cock <alexcock27@gmail.com>
2023-12-26 17:49:09 +0000

    DM/Group/Channel checks done, also searches for URLs

--- BotMaster.py
+++ BotMaster.py
@@ -1,5 +1,5 @@
 #Master Control File 
-
+#6460026166:AAFf7Wv93Cpa9YUFtsZ0E-AJfmFgKGDx_1s
 #Setup relevant imports from other API interfaces that will be required. 
 from telegram import Update
 from telegram.ext import MessageHandler, filters, ApplicationBuilder, ContextTypes
@@ -8,17 +8,14 @@ import asyncio
 import os 
 
 #Import the two other API interfaces
-from PTBot import Mod2
+from PTBot import Mod3
 #from VTBot import whoops
 
-#Main function
-async def main():
-    #Setup the bot, supply it with the desired token
-    PTBot = await Mod2.BotSetup('6460026166:AAFf7Wv93Cpa9YUFtsZ0E-AJfmFgKGDx_1s')
-    PTBot
-    
+API_TOKEN = "6460026166:AAFf7Wv93Cpa9YUFtsZ0E-AJfmFgKGDx_1s"
 
 
+"""
 #Execute main
 if __name__ == "__main__":
     asyncio.run(main())
+"""
--- PTBot/Mod3.py
+++ PTBot/Mod3.py
@@ -2,42 +2,107 @@ from telegram import Update
 from telegram.ext import MessageHandler, filters, ApplicationBuilder, ContextTypes
 
 import os
+import re
 
-mainDirectory = "/home/alexander/FYP-Crawler GitRepo/FYP-Crawler/"
+mainDirectory = "/home/alexander/FYP-Crawler GitRepo/FYP-Crawler/Data"
 
-def checkIfExistent(titleOfRoom):
-    if os.path.exists(mainDirectory+titleOfRoom):
+#Series of functions to create the relevant files in the data folder. 
+
+def appendTxt(titleOfRoom, message):
+    file = open(mainDirectory+"/"+titleOfRoom+"-Messages.txt", 'a')
+    file.write(message + "\n")
+    file.close() 
+
+def makeTxt(titleOfRoom):
+    file = open(mainDirectory+"/"+titleOfRoom+"-Messages.txt", 'x')
+    file.close() 
+
+def checkFileExsits(titleOfRoom): #Check if the txt already exists
+    status = os.path.exists(mainDirectory+"/"+titleOfRoom+"-Messages.txt")
+    if status == True:    
         return True
-    else:
+    elif status == False: 
         return False
-    
-def mkdir(titleOfRoom):
-    os.mkdir(mainDirectory+titleOfRoom)
-    open(mainDirectory+titleOfRoom+".txt", "x")
-    newDir = mainDirectory+titleOfRoom+"/"+titleOfRoom+".txt"
-    print(newDir)
-    return newDir
+    else:
+        print("checkDirExists Broke! Assume False!")
+        return False
 
-def appendToFile(dir,message):
-    file = open(dir,"a")
-    file.write(message+"\n")
-    file.close()
+    #What gets passed to the main program - uses all of the above.    
+def fileCheckConditional(roomTitle, message):
+    if checkFileExsits(roomTitle) == True:
+        appendTxt(roomTitle, message)
+    elif checkFileExsits(roomTitle) == False:
+        makeTxt(roomTitle)
+        appendTxt(roomTitle, message)
+
+"""
+    Now for URLS! 
+    Could this be more efficient and be merged? Yes.
+    Do I give a fuck? No. 
+"""
+def appendURLTxt(titleOfRoom, message):
+    file = open(mainDirectory+"/"+titleOfRoom+"-URLs.txt", 'a')
+    file.write(message + "\n")
+    file.close() 
+
+def makeURLTxt(titleOfRoom):
+    file = open(mainDirectory+"/"+titleOfRoom+"-URLs.txt", 'x')
+    file.close() 
+
+def checkURLFileExsits(titleOfRoom): #Check if the URL txt already exists
+    status = os.path.exists(mainDirectory+"/"+titleOfRoom+"-URLs.txt")
+    if status == True:    
+        return True
+    elif status == False: 
+        return False
+    else:
+        print("checkDirExists Broke! Assume False!")
+        return False
+
+    #Uses above for URLs specifically. 
+def URLCheckConditional(roomTitle, message):
+    if checkURLFileExsits(roomTitle) == True:
+        appendURLTxt(roomTitle, message)
+    elif checkURLFileExsits(roomTitle) == False:
+        makeURLTxt(roomTitle)
+        appendURLTxt(roomTitle, message)
+
+#Checks to see if a DM or other form of chat, as DM's don't have titles. 
+def checkUpdateType(update):
+    if update.effective_chat.type == "private":
+        return update.effective_chat.first_name
+    else: 
+        return update.effective_chat.title
+
+#Scan for a URL in a message
+def URLScanMessage(message):
+    URLs = re.findall(r'(https?://[^\s]+)', message)
+    if URLs: #If there was a URL in the message
+        return True, URLs #Return true and the list
+    else: #Else assume no URLs, return false and the empty list
+        return False, URLs
+
+    """
+    TODO: 
+    Check if a DM or a group/channel update! CHECK
+    Check for URL in messages! 
+    Finish VTScripts!
+    Setup asyncio communication with VT
+    """
 
 #Reads any messages in an update
 async def ReadAllMsgs(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update._effective_message.text
-    roomTitle = update.effective_chat.title
-
-    if checkIfExistent(roomTitle) == False:
-        newDir = mkdir(roomTitle)
-        appendToFile(newDir, message)
-    elif checkIfExistent(roomTitle) == True:
-        appendToFile(mainDirectory+roomTitle+"/"+roomTitle+".txt", message)
-
+    print(message)
+    roomTitle = checkUpdateType(update)
+    print(roomTitle)
+    fileCheckConditional(roomTitle,message)
+    bool, urlList = URLScanMessage(message)
+    if bool == True:
+        for item in urlList:
+            URLCheckConditional(roomTitle,item)
     else:
-        print("Shits fucked!")
-        print(message)
-        print(roomTitle)
+        print("Do Nothing")
 
 #Grabs a file from any update
 async def GrabFile(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -48,20 +113,6 @@ async def GrabFile(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def ExportHistory():
     print("History Exported!")
-
-#Supply with token in main, build bot
-async def BotSetup(API_TOKEN):
-    PTBot = ApplicationBuilder().token(
-        API_TOKEN
-    ).build()
-
-    file_handler = MessageHandler(filters.ATTACHMENT & (~filters.COMMAND), GrabFile)
-    channel_handler = MessageHandler(filters.ALL & (~filters.COMMAND), ReadAllMsgs)
-
-    PTBot.add_handler(file_handler)
-    PTBot.add_handler(channel_handler)
-    #return the built bot with handlers attached
-    return PTBot
 
 #Boilerplate to stop script running when imported
 if __name__ == '__main__':
